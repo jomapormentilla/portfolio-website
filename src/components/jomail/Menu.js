743434af11b6ce3bea11f9e54e9005eabcb060ab
jomapormentilla <jomapormentilla@gmail.com>
2021-09-03 16:17:19 -0400

    Implement 'set active' functionality when clicking on different menu items.

--- src/components/jomail/Menu.js
+++ src/components/jomail/Menu.js
@@ -3,12 +3,12 @@ import React from 'react'
 class Menu extends React.Component {
     state = {
         items: [
-            { name: 'Inbox', icon: 'bi-inbox', active: true },
+            { name: 'Inbox', icon: 'bi-inbox-fill', active: true },
             { name: 'Starred', icon: 'bi-star-fill', active: false },
             { name: 'Important', icon: 'bi-cone-striped', active: false },
             { name: 'Sent', icon: 'bi-cursor-fill', active: false },
-            { name: 'Drafts', icon: 'bi-file-earmark', active: false },
-            { name: 'Spam', icon: 'bi-exclamation-octagon', active: false },
+            { name: 'Drafts', icon: 'bi-file-earmark-fill', active: false },
+            { name: 'Spam', icon: 'bi-exclamation-octagon-fill', active: false },
             { name: 'Trash', icon: 'bi-trash-fill', active: false },
         ]
     }
@@ -19,14 +19,28 @@ class Menu extends React.Component {
             if ( !!item.active ) {
                 active = ' menu-jomail-active'
             }
-            return <div key={ id } className={ active }><i className={ item.icon }></i> &nbsp; { item.name }</div>
+            return <div key={ id } data-id={ item.name } className={ active } onClick={ this.setActive } title={ item.name }><i className={ item.icon }></i> &nbsp; { item.name }</div>
+        })
+    }
+
+    setActive = e => {
+        this.setState(prevState => {
+            for (const el in prevState.items) {
+                if (prevState.items[el].name === e.target.dataset.id) {
+                    prevState.items[el].active = true
+                } else {
+                    prevState.items[el].active = false
+                }
+            }
+
+            return prevState
         })
     }
 
     render(){
         return(
             <div className="menu-jomail">
-                <button className="btn btn-light" style={{ 'margin': '10px', 'padding': '10px', 'marginBottom': '20px' }}>Compose</button>
+                <button className="btn btn-light" style={{ 'margin': '10px', 'padding': '10px', 'marginBottom': '20px', 'fontSize': '15px' }}>Compose</button>
                 { this.renderItems() }
             </div>
         )
--- src/components/jomail/MessagesTabs.js
+++ src/components/jomail/MessagesTabs.js
@@ -3,16 +3,33 @@ import React from 'react'
 class MessagesTabs extends React.Component {
     state = {
         tabs: [
-            {   name: 'Primary', icon: 'bi-inbox' },
-            {   name: 'Social', icon: 'bi-people' },
-            {   name: 'Promotions', icon: 'bi-tag' },
-            {   name: 'Updates', icon: 'bi-info-circle' },
+            {   name: 'Primary', icon: 'bi-inbox', active: true },
+            {   name: 'Social', icon: 'bi-people', active: false },
+            {   name: 'Promotions', icon: 'bi-tag', active: false },
+            {   name: 'Updates', icon: 'bi-info-circle', active: false },
         ]
     }
 
     renderTabs = () => {
         return this.state.tabs.map((tab, id) => {
-            return <div key={ id }><i className={ tab.icon }></i> &nbsp; { tab.name }</div>
+            let active = ''
+            if (!!tab.active) {
+                active = 'messages-tabs-active'
+            }
+            return <div key={ id } className={ active } data-id={ tab.name } onClick={ this.setActive }><i className={ tab.icon }></i> &nbsp; { tab.name }</div>
+        })
+    }
+
+    setActive = e => {
+        this.setState(prevState => {
+            for (const el in prevState.tabs) {
+                if (prevState.tabs[el].name === e.target.dataset.id) {
+                    prevState.tabs[el].active = true
+                } else {
+                    prevState.tabs[el].active = false
+                }
+            }
+            return prevState
         })
     }
 
