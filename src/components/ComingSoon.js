a45dc495df6afec817d8663356652de0322419ad
jomapormentilla <jomapormentilla@gmail.com>
2021-09-02 20:20:52 -0400

    Testing CI/CD Pipeline.

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -6,6 +6,8 @@ class ComingSoon extends React.Component {
             <div className="coming-soon" style={{ 'flexDirection': 'column' }}>
                 <h1>The Jomatrix Portfolio Site</h1>
                 <h2>Coming Soon...</h2>
+                <hr />
+                <h3>[[ Testing CI/CD Pipeline ]]</h3>
             </div>
         )
     }
